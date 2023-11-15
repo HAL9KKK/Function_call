45b0b6034b058caadf8448dbc55d6b9a912860e0
HAL9KKK <63504776+HAL9KKK@users.noreply.github.com>
2023-11-15 18:43:15 +0100

    Update openai_api.py

--- openai_api_demo/openai_api.py
+++ openai_api_demo/openai_api.py
@@ -232,8 +232,8 @@ if __name__ == "__main__":
 
     model_path = "THUDM/chatglm3-6b"
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
-
+    #model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
+    model = AutoModel.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True, device='cuda')
     # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm3-6b", num_gpus=2)
